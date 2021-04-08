8ca69629f0d8528d228ec913897d7407ea8da3f5
Jonathan Corley <>
2021-04-08 15:47:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//DO NOT CHANGE CODE HERE
+var customers = require('./customers.json');
+//DO NOT CHANGE CODE HERE
+
+
+
+
+/*
+1. Return the all friends of a given customers name.
+When returning the friends of the customer it
+should be a string and NOT the friends array. 
+*/
+
+function friendsOfCustomer(name){
+  //CODE BELOW HERE
+
+
+
+  //CODE ABOVE HERE
+}
+
+
+
+
+
+/*
+2. Return the phone number and address of a 
+given customer when thier name is entered into the 
+function.
+*/
+
+function contactInfo(name){
+  //CODE BELOW HERE
+
+
+
+  //CODE ABOVE HERE
+}
